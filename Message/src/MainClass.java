ff09c70d5a7610d2ccb70f79225f6ffe4d0b78b0
Bharath U <umashankarbharath8@gmail.com>
2018-07-26 11:03:18 +0545

    toString

---
+++ Message/src/MainClass.java
@@ -0,0 +1,14 @@
+
+public class MainClass {
+	
+	public static void main(String[] args) {
+		Point p1 = new Point(2,4);
+		
+		String str;
+		str="x & y : "+ p1;
+		
+		System.out.println(str);
+		
+	}
+
+}
